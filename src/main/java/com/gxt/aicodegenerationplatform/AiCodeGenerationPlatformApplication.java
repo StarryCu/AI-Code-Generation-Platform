f3498029b9b176db721c70112014758f99f0bcb1
2301_80634680 <1750758616@qq.com>
2026-04-20 16:15:46 +0800

    创建项目

---
+++ src/main/java/com/gxt/aicodegenerationplatform/AiCodeGenerationPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.gxt.aicodegenerationplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiCodeGenerationPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiCodeGenerationPlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gxt/aicodegenerationplatform/AiCodeGenerationPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gxt.aicodegenerationplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiCodeGenerationPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
